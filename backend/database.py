d6c03221805d6dd55e9cc9eea834226031cd04f5
habiburahmanrulialif <habibalif865@gmail.com>
2024-07-07 23:08:35 +0700

    change url database

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 
-URL_DATABASE = "postgresql+asyncpg://postgres:test123@172.18.0.1:5432/feedbackDb"
+URL_DATABASE = "postgresql+asyncpg://postgres:test123@db:5432/feedbackDb"
 
 engine = create_async_engine(URL_DATABASE)
 SessionLocal = async_sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
